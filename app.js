38df430ac9dd553be0df8d01ae6dda87052638ba
V0we <110998036+V0we@users.noreply.github.com>
2024-09-19 22:47:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Variables
 
 let heading = document.getElementsByClassName('heading');
-let answer = document.getElementsByClassName('answer')
+
 
 for (i = 0; i < heading.length; i++) {
     heading[i].addEventListener("click", function() {
